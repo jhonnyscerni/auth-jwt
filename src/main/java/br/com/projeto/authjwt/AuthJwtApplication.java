666175fcde15bc2792e61054f3782a5ca7996298
jhonnyscerni <jhonnyscerni@gmail.com>
2022-05-04 02:33:28 -0300

    - Cross Cutting Service Registry Discovery Pattern

--- src/main/java/br/com/projeto/authjwt/AuthJwtApplication.java
+++ src/main/java/br/com/projeto/authjwt/AuthJwtApplication.java
@@ -3,9 +3,11 @@ package br.com.projeto.authjwt;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @OpenAPIDefinition
+@EnableEurekaClient
 public class AuthJwtApplication {
 
     public static void main(String[] args) {
